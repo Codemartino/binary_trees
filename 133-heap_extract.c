4d663cb950a8f2ed45ed9b4511999a50e547ad82
Valentine1244 <59285068+Valentine1244@users.noreply.github.com>
2023-08-31 21:26:30 +0100

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -5,7 +5,7 @@
  * tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  *
- * Martin
+ * valentine
  * Return: Height or 0 if tree is NULL
  */
 size_t tree_height(const heap_t *tree)
