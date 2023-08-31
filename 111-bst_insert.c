bf747f993e543e82a05545a9c774bdd55ce0bb71
Valentine1244 <59285068+Valentine1244@users.noreply.github.com>
2023-08-31 21:29:38 +0100

    Update BST - Insert

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -4,7 +4,7 @@
  * bst_insert - Inserts a value in a Binary Search Tree.
  * @tree: A double pointer to the root node of the BST to insert the value.
  * @value: The value to store in the node to be inserted.
- * Akudugu
+ * Akamigbo
  *
  * Return: A pointer to the created node, or NULL on failure.
  */
